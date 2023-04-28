f941e57e38812676a4f21c6e1b5e6cd151f0454c
Brooke Bryan <brooke@bajb.net>
2023-04-28 10:14:02 +0100

    Anonymous configured

--- providers/anonymous/configure.go
+++ providers/anonymous/configure.go
@@ -1,7 +1,14 @@
 package anonymous
 
+import "encoding/json"
+
 const ProviderKey = "anonymous"
 
 func FromJson(data []byte) (*Provider, error) {
-	return &Provider{}, nil
+	prov := &Provider{}
+	if err := json.Unmarshal(data, prov); err == nil {
+		return prov, nil
+	} else {
+		return nil, err
+	}
 }
